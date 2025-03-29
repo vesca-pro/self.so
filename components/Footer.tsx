add34c3d14abb947e58f67b93aae7f693de5c3b5
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2025-03-29 14:29:56 +0100

    refactor(Footer): replace Link with a tags for external links
    Using `<a>` tags instead of `next/link` for external links ensures proper behavior with `target="_blank"` and `rel="noopener noreferrer"` attributes, improving security and user experience.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,35 +1,38 @@
-import Link from "next/link";
-
 export function Footer() {
   return (
     <footer className="w-full py-4 px-6  mt-auto border-t border-gray-200">
       <div className="max-w-4xl justify-between items-center mx-auto w-full flex flex-col-reverse md:flex-row gap-2">
         <div className="text-sm text-design-gray font-mono font-bold">
           Powered by{" "}
-          <Link
+          <a
+            target="_blank"
             href="https://together.ai?ref=selfso"
             className="text-design-black underline underline-offset-2"
           >
             Together.ai
-          </Link>{" "}
+          </a>{" "}
           & <span className="text-design-black">Llama 3.1</span>
         </div>
 
         <div className="flex gap-2">
-          <Link
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
             href="https://github.com"
             className="size-6 flex items-center justify-center border-design-gray border rounded-md"
           >
             <img src="/footer/github.svg" className="size-4" />
             <span className="sr-only">GitHub</span>
-          </Link>
-          <Link
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
             href="https://x.com"
             className="size-6 flex items-center justify-center border-design-gray border rounded-md"
           >
             <img src="/footer/x.svg" className="size-4" />
             <span className="sr-only">Social</span>
-          </Link>
+          </a>
         </div>
       </div>
     </footer>
