db2c6cfa2491d8801505173d6aafcb542857910a
Hassan El Mghari <hassan4709@gmail.com>
2025-04-02 12:48:23 -0400

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type React from "react";
-import { JetBrains_Mono } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
-import { ReactQueryClientProvider } from "@/components/ReactQueryClientProvider";
-import { Metadata } from "next";
+import type React from 'react';
+import { JetBrains_Mono } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+import './globals.css';
+import { Toaster } from '@/components/ui/sonner';
+import { ReactQueryClientProvider } from '@/components/ReactQueryClientProvider';
+import { Metadata } from 'next';
 
-const mono = JetBrains_Mono({ subsets: ["latin"] });
+const mono = JetBrains_Mono({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://self-so.vercel.app"),
-  title: "Self.so - Resume to Website",
+  metadataBase: new URL('https://self-so.vercel.app'),
+  title: 'Self.so - Resume to Website',
   description:
-    "LinkedIn to Website in one click! Powered by Together AI and Llama 3.1",
+    'LinkedIn to Website in one click! Powered by Together AI and Llama 3.1',
   openGraph: {
-    images: "/og.png",
+    images: '/og.png',
   },
 };
 
@@ -26,19 +26,19 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <ReactQueryClientProvider>
-        <html lang="en">
+        <html lang='en'>
           <head>
-            {process.env.NODE_ENV === "development" && (
+            {/* {process.env.NODE_ENV === "development" && (
               <script
                 crossOrigin="anonymous"
                 src="//unpkg.com/react-scan/dist/auto.global.js"
               />
-            )}
+            )} */}
             {/* rest of your scripts go under */}
           </head>
           <body className={`${mono.className} min-h-screen flex flex-col`}>
-            <main className="flex-1 flex flex-col">{children}</main>
-            <Toaster richColors position="bottom-center" />
+            <main className='flex-1 flex flex-col'>{children}</main>
+            <Toaster richColors position='bottom-center' />
           </body>
         </html>
       </ReactQueryClientProvider>
