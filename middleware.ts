4250af7d5f696b8711b8dbc1664157aa58c28203
Hassan El Mghari <hassan4709@gmail.com>
2025-04-02 19:20:28 -0400

    fixed auth

--- middleware.ts
+++ middleware.ts
@@ -1,67 +1,16 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
-import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { PRIVATE_ROUTES } from './lib/routes';
 
-const trustedCrawlers = [
-  'Googlebot',
-  'Bingbot',
-  'Slurp',
-  'DuckDuckBot',
-  'Baiduspider',
-  'YandexBot',
-  'Facebot',
-  'Twitterbot',
-  'Slackbot-LinkExpanding',
-  'Applebot',
-  'OpenGraphing',
-];
+const isPrivateRoute = createRouteMatcher(
+  PRIVATE_ROUTES.map((route) => `/${route}`)
+);
 
-export default function middleware(
-  request: NextRequest,
-  event: NextFetchEvent
-) {
-  const userAgent = request.headers.get('user-agent') || '';
-
-  // Function to check if the request comes from a trusted crawler
-  const isCrawler = trustedCrawlers.some((crawler) =>
-    userAgent.includes(crawler)
-  );
-
-  if (
-    !isCrawler &&
-    request.nextUrl.pathname.startsWith('/') &&
-    process.env.NODE_ENV === 'production'
-  ) {
-    const basicAuth = request.headers.get('authorization');
-
-    if (!basicAuth) {
-      return new NextResponse('Auth required', {
-        headers: {
-          'WWW-Authenticate': 'Basic realm="Secure Area"',
-        },
-        status: 401,
-      });
-    } else {
-      const authValue = basicAuth.split(' ')[1];
-      const [user, pwd] = atob(authValue).split(':');
-
-      if (user !== 'togetherai' || pwd !== '7093ther') {
-        return new NextResponse('Auth required', {
-          headers: {
-            'WWW-Authenticate': 'Basic realm="Secure Area"',
-          },
-          status: 401,
-        });
-      }
-    }
-
-    // This is apparently used by Clerk, but we're using it for basic HTTP auth
-    // to keep the site hidden for now. So, we delete after verifying it so that
-    // Clerk's middleware functions correctly.
-    request.headers.delete('Authorization');
+export default clerkMiddleware(async (auth, req) => {
+  // Protect all private routes - require authentication
+  if (isPrivateRoute(req)) {
+    await auth.protect();
   }
-
-  return clerkMiddleware()(request, event);
-}
+});
 
 export const config = {
   matcher: [
